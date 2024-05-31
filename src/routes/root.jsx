c5a710b9badb2d4c0c95547227fcc1cee96d16c1
jeremias256 <jere.menacho@gmail.com>
2024-05-31 18:52:44 -0300

    Active Link Styling - Global Pending UI - navlinks, loader contact

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink, useNavigation } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -14,6 +14,7 @@ export async function loader() {
 
 export default function Root() {
 	const { contacts } = useLoaderData();
+	const navigation = useNavigation();
 	return (
 		<>
 			<div id='sidebar'>
@@ -48,7 +49,9 @@ export default function Root() {
 							<ul>
 								{contacts.map((contact) => (
 									<li key={contact.id}>
-										<Link to={`contacts/${contact.id}`}>
+										<NavLink
+											to={`contacts/${contact.id}`}
+											className={({ isActive, isPending }) => (isActive ? "active" : isPending ? "pending" : "")}>
 											{contact.first || contact.last ? (
 												<>
 													{contact.first} {contact.last}
@@ -57,7 +60,7 @@ export default function Root() {
 												<i>No Name</i>
 											)}{" "}
 											{contact.favorite && <span>★</span>}
-										</Link>
+										</NavLink>
 									</li>
 								))}
 							</ul>
@@ -69,7 +72,9 @@ export default function Root() {
 					</ul>
 				</nav>
 			</div>
-			<div id='detail'>
+			<div
+				id='detail'
+				className={navigation.state === "loading" ? "loading" : ""}>
 				<Outlet />
 			</div>
 		</>
