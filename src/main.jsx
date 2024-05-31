5b872113690b1162c457a79ff54bd5463b89ee9a
jeremias256 <jere.menacho@gmail.com>
2024-05-31 17:45:38 -0300

    Updating Data - edit page

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./error-page";
-import Contact, { loader as contactLoader } from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
 	{
@@ -17,6 +18,11 @@ const router = createBrowserRouter([
 			{
 				path: "contacts/:contactId",
 				element: <Contact />,
+				loader: contactLoader,
+			},
+			{
+				path: "contacts/:contactId/edit",
+				element: <EditContact />,
 				loader: contactLoader,
 			},
 		],
