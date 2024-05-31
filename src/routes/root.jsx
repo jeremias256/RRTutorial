f099dabb17aad2d8524a749727e2f3cf2c683f2e
jeremias256 <jere.menacho@gmail.com>
2024-05-31 16:35:16 -0300

    Loading Data - cargar contactos de bd

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,13 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contacts";
+
+export async function loader() {
+	const contacts = await getContacts();
+	return { contacts };
+}
 
 export default function Root() {
+	const { contacts } = useLoaderData();
 	return (
 		<>
 			<div id='sidebar'>
@@ -31,12 +38,28 @@ export default function Root() {
 				</div>
 				<nav>
 					<ul>
-						<li>
-							<Link to={`contacts/1`}>Your Name</Link>
-						</li>
-						<li>
-							<Link to={`contacts/2`}>Your Friend</Link>
-						</li>
+						{contacts.length ? (
+							<ul>
+								{contacts.map((contact) => (
+									<li key={contact.id}>
+										<Link to={`contacts/${contact.id}`}>
+											{contact.first || contact.last ? (
+												<>
+													{contact.first} {contact.last}
+												</>
+											) : (
+												<i>No Name</i>
+											)}{" "}
+											{contact.favorite && <span>★</span>}
+										</Link>
+									</li>
+								))}
+							</ul>
+						) : (
+							<p>
+								<i>No hay contactos</i>
+							</p>
+						)}
 					</ul>
 				</nav>
 			</div>
