f6829713fe46c06f4f4c6c94b11b4db434d219f2
jeremias256 <jere.menacho@gmail.com>
2024-05-31 21:07:58 -0300

    multiples children - agregar sidebar al errorPage

--- src/main.jsx
+++ src/main.jsx
@@ -13,27 +13,31 @@ const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Root />,
-		errorElement: <ErrorPage />,
 		loader: rootLoader,
 		action: rootAction,
 		children: [
-			{ index: true, element: <Index /> },
 			{
-				path: "contacts/:contactId",
-				element: <Contact />,
-				loader: contactLoader,
-				action: contactAction,
-			},
-			{
-				path: "contacts/:contactId/edit",
-				element: <EditContact />,
-				loader: contactLoader,
-				action: editAction,
-			},
-			{
-				path: "contacts/:contactId/destroy",
-				action: destroyAction,
-				errorElement: <div>Oops! There was an error.</div>,
+				errorElement: <ErrorPage />,
+				children: [
+					{ index: true, element: <Index /> },
+					{
+						path: "contacts/:contactId",
+						element: <Contact />,
+						loader: contactLoader,
+						action: contactAction,
+					},
+					{
+						path: "contacts/:contactId/edit",
+						element: <EditContact />,
+						loader: contactLoader,
+						action: editAction,
+					},
+					{
+						path: "contacts/:contactId/destroy",
+						action: destroyAction,
+						errorElement: <div>Oops! There was an error.</div>,
+					},
+				],
 			},
 		],
 	},
