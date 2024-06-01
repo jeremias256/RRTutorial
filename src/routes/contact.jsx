cb48bf546eaa74a6a03005775980a4c96ab08f6e
jeremias256 <jere.menacho@gmail.com>
2024-05-31 21:05:55 -0300

    Not Found Data - no existe contacto

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -28,6 +28,12 @@ function Favorite({ contact }) {
 
 export async function loader({ params }) {
 	const contact = await getContact(params.contactId);
+	if (!contact) {
+		throw new Response("", {
+			status: 404,
+			statusText: "Not Found",
+		});
+	}
 	return { contact };
 }
 
